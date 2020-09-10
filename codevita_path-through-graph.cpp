b853732da9ce3881e249c4ccba7cf5cb91886c70
Spandan Banerjee <36163778+sban2009@users.noreply.github.com>
2020-09-10 17:08:44 +0530

    Change implementation from vector to map
    O(n) complexity in vector reduced to O(1) search in hashmap

--- codevita_path-through-graph.cpp
+++ codevita_path-through-graph.cpp
@@ -5,114 +5,89 @@ Path Through Graph
 You are given two natural numbers.
 Imagine these natural numbers as nodes on a graph.
 On this graph, a number is connected to its largest factor other than itself.
-You have to find the shortest path between them.
+You have to find the #edges in the shortest path between them.
 If the two numbers do not have any common factor, then construct a path through 1.
 
-Ex 1: Input: 16 4
+Ex 1: 16 4
 Since we know that largest factor of 16 other than itself is 8.
 Largest factor of 8 other than itself is 4. That’s the other input number, so
 we will stop here.
-Output: 16 <--> 8 <--> 4
+16 <--> 8 <--> 4
+Ans: 2
 
-Ex 2: Input: 15689 28
+Ex 2: 15689 28
 Since we know that largest factor of 15689 other than itself is 541.
 Since 541 is a prime number, it’s largest factor other than itself is 1.
-For number 28, it’s largest factor other than itself is 14.
+For number 28, its largest factor other than itself is 14.
 Largest factor of 14, other than itself is 7.
-Since 7 is a prime number, it’s largest factor other than itself is 1.
-Output: 15689 <--> 541 <--> 1 <--> 7 <--> 14 <--> 28
+Since 7 is a prime number, its largest factor other than itself is 1.
+15689 <--> 541 <--> 1 <--> 7 <--> 14 <--> 28
+Ans: 5
 */
 #include <bits/stdc++.h>
 using namespace std;
-//input (x,y) example: (72,6) or (6,72)
-vector<int> path_between(int x, int y)
+int factor(int n)
 {
-	//keep x>y
-	//(6,72) becomes (72,6)
-	if (x < y)
-		swap(x, y);
-	//graph for x factors
-	vector<int> graph;
-	graph.emplace_back(x);
-	bool prime, done = false; //prime = if number is prime\
-							done=if graph complete (optional)
-	while (x != 1)
+	for (int i = 2; i * i <= n; i++) //finding largest factor other than number itself
 	{
-		prime = true;
-		if (x == y) //y is one of the nodes
-		{
-			done = true;
-			break;
-		}
-		//finding largest factor other than number itself
-		for (int i = 2; i <= (int)sqrt(x); i++)
-		{
-			if (x % i == 0)
-			{
-				prime = false;
-				x /= i;
-				graph.emplace_back(x);
-				break;
-			}
-		}
-		if (prime)
-		{
-			x = 1;
-			graph.emplace_back(x);
-			break;
-		}
+		if (n % i == 0) // divide by lowest prime factor and return
+			return n / i;
 	}
-	if (done) //y occurs in the graph already
-		return graph;
-	//graph: 72 -> 36 -> 18 -> 9 -> 3 -> 1
-	//graph for y factors
-	vector<int> graph2;
-	graph2.emplace_back(y);
-	//same process as graph for x
-	while (y != 1)
-	{
-		prime = true;
-		for (int i = 2; i <= (int)sqrt(y); i++)
-		{
-			if (y % i == 0)
-			{
-				prime = false;
-				y /= i;
-				graph2.emplace_back(y);
-				break;
-			}
-		}
-		if (prime)
-			break; //1 is not inserted as already available\
-					in previous graph
-	}
-	//graph2: 6 -> 3
-	//reverse graph for y
-	reverse(graph2.begin(), graph2.end());
-	//graph2 reversed: 3 -> 6
-	graph.insert(graph.end(), graph2.begin(), graph2.end());
-	//graph = (graph)->(graph2 reversed)\
-		i.e. (72 -> 36 -> 18 -> 9 -> 3 -> 1) -> (3 -> 6)\
-			graph2 reversed and appended to end of graph
-	return graph;
+	return 1; // number is prime
 }
 int main()
 {
-	int a, b;
+	int x, y;
 	cout << "enter 1st num: ";
-	cin >> a;
+	cin >> x;
 	cout << "enter 2nd num: ";
-	cin >> b;
-	vector<int> graph = path_between(a, b);
-	for (auto i = graph.begin(); i != graph.end(); i++)
+	cin >> y;
+	if (x < y) // keeping first number larger
+		swap(x, y);
+	if (x == y) // numbers are equal. no edges
 	{
-		if (i + 1 != graph.end())
-			cout << *i << " <--> ";
-		else
-			cout << *i;
+		cout << 0;
+		return 0;
 	}
-	//for original problem, #edges required\
-	#edges=#nodes-1, i.e. cout<<graph.size()-1
+	map<int, int> graph; // stores x factors, with #edges
+	int c = 0;
+	while (x != 1) // x not prime
+	{
+		c++;
+		x = factor(x);
+		graph[x] = c; // insert <node,#edges>
+	}
+	c = 0;
+	while (!graph.count(y)) // y not present in graph
+	{
+		c++;
+		y = factor(y);
+	}
+	// #total edges = #(x factors till y link) + #(y link to y)
+	cout << "#edges: "<<graph[y] + c << "\n";
+	return 0;
 }
-//output: 72 <--> 36 <--> 18 <--> 9 <--> 3 <--> 1 <--> 3 <--> 6
-//original o/p: 7
+/*
+example: 
+enter 1st num: 54
+enter 2nd num: 36
+#edges: 4
+
+explanation:
+map looks like
+27: 1, 9: 2, 3: 3, 1: 4
+#edges in x graph = 4
+54 <--> 27 <--> 9 <--> 3 <--> 1
+
+when finding factors of 36, 9 is common
+the loop runs as:
+36 not in graph-> c=1, 18 not in graph-> c=2, 9 in graph. terminated.
+#edges in y graph = c = 2
+36<-->18<-->9
+
+#edges in total graph = #edges till 9 in x graph + #edges in y graph
+= map(9) + 2
+= 2 + 2
+= 4
+54 <--> 27 <--> 9 <--> 18 <--> 36
+*/
