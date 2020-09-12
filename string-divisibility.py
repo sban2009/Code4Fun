5cd4b31e7b64fb5d3f7970f0bf7f0fac1c2b6567
Spandan Banerjee <36163778+sban2009@users.noreply.github.com>
2020-09-13 03:43:18 +0530

    Code formatting

--- string-divisibility.py
+++ string-divisibility.py
@@ -11,25 +11,31 @@ EXAMPLE:
 i/p: s="abcabcabcabc" t="abcabc"
 o/p: 3 (AS |u|=|"abc"|=3)
 '''
-def FSD2(s,t):
-    temp=""
-    for i in range(0,int(len(s)/len(t))):
-        temp+=t
-        if(temp==s):
+
+
+def FSD2(s, t):
+    temp = ""
+    for i in range(0, int(len(s)/len(t))):
+        temp += t
+        if(temp == s):
             return True
     return False
-def FSD(s,t):
-    temp1=""
-    temp2=""
-    for i in range(0,int(len(s)/len(t))):
-        temp1+=t
-        if(temp1==s):
-            for j in range(0,len(t)):
-                u=t[0:j+1]
-                if(FSD2(t,u)):
+
+
+def FSD(s, t):
+    temp1 = ""
+    temp2 = ""
+    for i in range(0, int(len(s)/len(t))):
+        temp1 += t
+        if(temp1 == s):
+            for j in range(0, len(t)):
+                u = t[0:j+1]
+                if(FSD2(t, u)):
                     return len(u)
             return len(t)
     return -1
+
+
 '''
 print(FSD("rbrb","rbrb"))
 print(FSD("ababab","abab"))
